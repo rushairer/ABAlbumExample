c79144e09719a5800b6ec07a6eba297f2a42e4aa
Abenx <5195693+rushairer@users.noreply.github.com>
2021-07-27 00:47:44 +0800

    Initial Commit

---
+++ Shared/ABAlbumExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABAlbumExampleApp.swift
+//  Shared
+//
+//  Created by Abenx on 2021/7/27.
+//
+
+import SwiftUI
+
+@main
+struct ABAlbumExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Abenx on 2021/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

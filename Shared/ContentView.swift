9a73db64153dc719c8d3e883efd1012ece2d5b02
Abenx <5195693+rushairer@users.noreply.github.com>
2021-07-27 01:28:47 +0800

    [Init]

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
+import ABAlbum
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AlbumPage()
     }
 }
 

52e86fcb4f52c11e703bbf7415f7599d11d95c46
Abenx <5195693+rushairer@users.noreply.github.com>
2021-07-31 20:17:17 +0800

    [Changed]Change documentation of album.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,27 +7,54 @@
 
 import SwiftUI
 import ABAlbum
+import Photos
 
 struct ContentView: View {
     @Environment(\.openURL) var openURL
+    
+    /// 是否显示权限提示页. 默认: 不显示.
+    @State private var showsAlbumNoPermissionView: Bool = AlbumService.shared.isNotDetermined
+    
+    init() {
+        let options = PHFetchOptions()
+        options.predicate = NSPredicate(format: "mediaType == %ld", PHAssetMediaType.image.rawValue)
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+
+        AlbumService.shared.collectionFetchOptions = options
+    }
+    
+    var albumPage: some View {
+        AlbumPage()
+            .showsNoPermissionView($showsAlbumNoPermissionView)
+            .task {
+                showsAlbumNoPermissionView = await !AlbumService.shared.hasAlbumPermission
+            }
+    }
     
     var body: some View {
         NavigationView {
             List {
                 Section {
-                    NavigationLink("Album", destination:  AlbumPage())
+                    NavigationLink("Album", destination: albumPage)
                     NavigationLink("Camera", destination:  CameraPage())
                 }
+#if !os(macOS)
+                
                 Section {
                     Button("Go to system settings") {
                         guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
                         openURL(url)
                     }
                 }
+#endif
+                
             }
+#if !os(macOS)
             .listStyle(.insetGrouped)
+#endif
             Text("Welcome")
         }
+        
     }
 }
 
